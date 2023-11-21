306c0bb5af140a24df4001309083fdb77478a07b
rominaguevara <rominaguevara2207@hotmail.com>
2023-11-20 21:28:27 -0500

    TALLER 5

--- src/main/java/com/uce/edu/Pa2U1P5RgApplication.java
+++ src/main/java/com/uce/edu/Pa2U1P5RgApplication.java
@@ -1,20 +1,31 @@
 package com.uce.edu;
 
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.uce.edu.ioc.di.Estudiante;
+
 @SpringBootApplication
 public class Pa2U1P5RgApplication implements CommandLineRunner {
 
+	@Autowired
+	private Estudiante estudiante;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Pa2U1P5RgApplication.class, args);
 	}
 	
 	public void run(String... args) throws Exception{
-		System.out.println("Hola Mundo ");
-		System.out.println("Soy Jose Jimenez y Romina Guevara");
-		System.out.println("Cambio en la misma Rama");
+		this.estudiante.setNombre("Alejandro");
+		this.estudiante.setApellido("Jimenez");
+		this.estudiante.setCedula("1719608729");
+		this.estudiante.setSalario(new BigDecimal(100));
+		
+		System.out.println(estudiante);
 	}
 }
 
