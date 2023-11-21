dea5a273dc8af93b5158e6fba114d0bcc0162956
rominaguevara <rominaguevara2207@hotmail.com>
2023-11-20 21:11:14 -0500

    taller 3

---
+++ src/main/java/com/uce/edu/Pa2U1P5RgApplication.java
@@ -0,0 +1,20 @@
+package com.uce.edu;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pa2U1P5RgApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U1P5RgApplication.class, args);
+	}
+	
+	public void run(String... args) throws Exception{
+		System.out.println("Hola Mundo ");
+		System.out.println("Soy Jose Jimenez y Romina Guevara");
+		System.out.println("Cambio en la misma Rama");
+	}
+}
+
